90a97b030920dcd23063e784674f942c989d8a50
Ben Mensah <benlottejnr@gmail.com>
2024-04-03 03:46:33 +0100

    minor update3

--- module/app1/main.tf
+++ module/app1/main.tf
@@ -4,6 +4,10 @@ module "key" {
   key_name = "benlottejnr"
 }
 
+
+
+
+/*
 module "vpc" {
     #it is a bad practice to include the link of terraform registry module
     #always clone the module and then modify, below is just for a quick work
@@ -12,7 +16,7 @@ module "vpc" {
   name = "my-vpc1234"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
@@ -25,3 +29,4 @@ module "vpc" {
   }
 }
 
+*/
