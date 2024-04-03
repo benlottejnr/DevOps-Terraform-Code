0aa2c6b26c0bc5d8e68a04a73fa0ef3c1947c320
Ben Mensah <benlottejnr@gmail.com>
2024-04-03 03:16:34 +0100

    main update 1

---
+++ data-source/data.tf
@@ -0,0 +1,23 @@
+data "aws_ami" "ami1" {
+  most_recent      = true
+  owners           = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+data "aws_instance" "ec21" {
+  instance_id = "i-0fea57c705cc3e31d"
+}
---
+++ import-code/import.tf
@@ -0,0 +1,4 @@
+import {
+  to = aws_instance.web
+  id = "i-0fea57c705cc3e31d"
+}
